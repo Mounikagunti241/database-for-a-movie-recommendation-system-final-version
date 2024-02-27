2f35af6e53ec98ab94343b4be92777701c848f14
Mounikagunti241 <156583406+Mounikagunti241@users.noreply.github.com>
2024-02-27 13:51:50 +0530

    Add files via upload

---
+++ dml_script_to_inserting_data_into_tables.sql
@@ -0,0 +1,246 @@
+----Inserting Sample Date In All Tables---------------
+-----------------------------CAST---------------------
+begin
+insert into cast values(seq_cast_id.nextval,'Mahesh Babu','Actor');
+insert into cast values(seq_cast_id.nextval,'Sreeleela','Actress');
+insert into cast values(seq_cast_id.nextval,'Prakash Raj','Villain');
+insert into cast values(seq_cast_id.nextval,'Ramaya Krishna','Actress');
+insert into cast values(seq_cast_id.nextval,'Brahmanandam','Comedian');
+insert into cast values(seq_cast_id.nextval,'Meenakshi Chaudhary','Female lead');
+insert into cast values(seq_cast_id.nextval,'Arjun Das','Villain');
+insert into cast values(seq_cast_id.nextval,'Nani','Actor');
+insert into cast values(seq_cast_id.nextval,'Prabhas','Actor');
+insert into cast values(seq_cast_id.nextval,'Teja Sajja','Hero');
+insert into cast values(seq_cast_id.nextval,'Ram charan ','Actor');
+insert into cast values(seq_cast_id.nextval,'Allu Arjun','Male Lead');
+insert into cast values(seq_cast_id.nextval,'Vijay','Actor');
+insert into cast values(seq_cast_id.nextval,'Vijay Sethupathi','negative lead');
+insert into cast values(seq_cast_id.nextval,'Malavika Mohanan','female lead');
+insert into cast values(seq_cast_id.nextval,'Dulquer Sulmaan','Actor');
+insert into cast values(seq_cast_id.nextval,'Rashmika Mandana','Heroine');
+insert into cast values(seq_cast_id.nextval,'Amritha Aiyer','Actress');
+insert into cast values(seq_cast_id.nextval,'Keerthy Suresh','Actress');
+insert into cast values(seq_cast_id.nextval,'Mrunal Thakar','Actress');
+insert into cast values(seq_cast_id.nextval,'Anushka Shetty','herione');
+insert into cast values(seq_cast_id.nextval,'Shraddha Srinath','Actress');
+insert into cast values(seq_cast_id.nextval,'AliaBhatt','Actress');
+end;
+
+-------------------------------CREW---------------------------
+begin
+insert into crew values(seq_crew_id.nextval,'Trivikram Srinivas','Director');
+insert into crew values(seq_crew_id.nextval,'Radha Krishna','Producer');
+insert into crew values(seq_crew_id.nextval,'S.Thaman','Musician');
+insert into crew values(seq_crew_id.nextval,'Manoj Paramahamsa','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'Navin Nooli','Editor');
+insert into crew values(seq_crew_id.nextval,'Gowtam Tinnanuri','Director');
+insert into crew values(seq_crew_id.nextval,'Dil Raju','Producer');
+insert into crew values(seq_crew_id.nextval,'Dasaradhi Sivendra','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'Mickey J.Mery','Musician');
+insert into crew values(seq_crew_id.nextval,'S S Rajamouli','Director');
+insert into crew values(seq_crew_id.nextval,'Shobu Yarlagadda','Producer');
+insert into crew values(seq_crew_id.nextval,'Shouryuv','Director');
+insert into crew values(seq_crew_id.nextval,'Miroslaw Brozek','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'C V Mohan','Producer');
+insert into crew values(seq_crew_id.nextval,'Priyanka Dutt','Producer');
+insert into crew values(seq_crew_id.nextval,'NagAshwin','Director');
+insert into crew values(seq_crew_id.nextval,'DeepaliNoor','Costume_Designer');
+insert into crew values(seq_crew_id.nextval,'AnirudthRavichander','Musician');
+insert into crew values(seq_crew_id.nextval,'K_Niranjan_Reddy','Producer');
+insert into crew values(seq_crew_id.nextval,'A.Sreekar prasad','Editor');
+insert into crew values(seq_crew_id.nextval,'Santhoshi','Costume_Designer');
+insert into crew values(seq_crew_id.nextval,'kk Senthil Kumar','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'MM Keeravani','Musician');
+insert into crew values(seq_crew_id.nextval,'Prasanth Varma','Director');
+insert into crew values(seq_crew_id.nextval,'S.B Raju Talari','Editor');
+insert into crew values(seq_crew_id.nextval,'Sheetal Sharama','Costume_Designer');
+insert into crew values(seq_crew_id.nextval,'C_Aswani Dutt','Producer');
+insert into crew values(seq_crew_id.nextval,'Hanu Raghavapudi','Director');
+insert into crew values(seq_crew_id.nextval,'Anil Bandari','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'Kotagiri Venkateshwara','Editor');
+insert into crew values(seq_crew_id.nextval,'v_vijayendra','Writer');
+insert into crew values(seq_crew_id.nextval,'Anudeep Dev','Musician');
+insert into crew values(seq_crew_id.nextval,'Rama_Rajamouli','Costume_Designer');
+insert into crew values(seq_crew_id.nextval,'Antony L.Ruben','Editor');
+insert into crew values(seq_crew_id.nextval,'Mahendra_gupta','Makeup_artist');
+insert into crew values(seq_crew_id.nextval,'Sathyan Sooryan','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'Sukumar','Director');
+insert into crew values(seq_crew_id.nextval,'Naveen Yerneni','producer');
+insert into crew values(seq_crew_id.nextval,'P.S_Vinod','Cinematographer');
+insert into crew values(seq_crew_id.nextval,'Philomin Raj','Editor');
+insert into crew values(seq_crew_id.nextval,'Amrita Ram','Costume_Designer');
+insert into crew values(seq_crew_id.nextval,'Devi Sri Prasad','Musician');
+insert into crew values(seq_crew_id.nextval,'Jagadish palanisamy','producer');
+insert into crew values(seq_crew_id.nextval,'Lokesh Kanajaraj','Director');
+insert into crew values(seq_crew_id.nextval,'D.V.V Danayya','producer');
+insert into crew values(seq_crew_id.nextval,'Lokesh Kanajaraj','Director');
+insert into crew values(seq_crew_id.nextval,'Vishal Chandrashekar','Musician');
+insert into crew values(seq_crew_id.nextval,'Allu Aravind','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Nagarjuna_Akkineni','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Daggubati Suresh','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Xavier Britto','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Vivek Kuchuilbhotla','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Vamsi Krishna ','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Gopi chand Achanta','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Anil sunkara','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'B.V.S.N prasad','productionHouseOwner');
+insert into crew values(seq_crew_id.nextval,'Vijendra Reddy Teegala','productionHouseOwner');
+end;
+
+-----------------------GENRE---------------------------
+begin
+insert  into genre values(seq_genre_id.nextval,'Action');
+insert  into genre values(seq_genre_id.nextval,'war');
+insert  into genre values(seq_genre_id.nextval,'Animation');
+insert  into genre values(seq_genre_id.nextval,'Thriller');
+insert  into genre values(seq_genre_id.nextval,'Drama');
+insert  into genre values(seq_genre_id.nextval,'Comedy');
+insert  into genre values(seq_genre_id.nextval,'Horror');
+insert  into genre values(seq_genre_id.nextval,'Fiction');
+insert  into genre values(seq_genre_id.nextval,'Crime');
+insert  into genre values(seq_genre_id.nextval,'History');
+insert  into genre values(seq_genre_id.nextval,'Documentry');
+insert  into genre values(seq_genre_id.nextval,'sports');
+insert  into genre values(seq_genre_id.nextval,'Narrative');
+insert  into genre values(seq_genre_id.nextval,'Experimetal');
+end;
+
+--------------------------PRODUCTION_HOUSE--------------------
+begin
+insert into production_House values(seq_production_hus_id.nextval,'Geetha Arts',148);
+insert into production_House values(seq_production_hus_id.nextval,'Annapurna Studios',149);
+insert into production_House values(seq_production_hus_id.nextval,'Mythri Movie Makers',137);
+insert into production_House values(seq_production_hus_id.nextval,'Arka Media Work',110);
+insert into production_House values(seq_production_hus_id.nextval,'Sri Venkateswara creation',106);
+insert into production_House values(seq_production_hus_id.nextval,'Suresh Production',150);
+insert into production_House values(seq_production_hus_id.nextval,'Vyjayanthi Movies',126);
+insert into production_House values(seq_production_hus_id.nextval,'DVV Entertainments',144);
+insert into production_House values(seq_production_hus_id.nextval,'XB Film Creators',151);
+insert into production_House values(seq_production_hus_id.nextval,'People Media Factory',152);
+insert into production_House values(seq_production_hus_id.nextval,'UV Creations',153);
+insert into production_House values(seq_production_hus_id.nextval,'14 Reels Entertainment',154);
+insert into production_House values(seq_production_hus_id.nextval,'AK Entertainments',155);
+insert into production_House values(seq_production_hus_id.nextval,'SVCC',156);
+insert into production_House values(seq_production_hus_id.nextval,'Harika&Hassine Creations',101);
+insert into production_House values(seq_production_hus_id.nextval,'Vyra Entertainments',157);
+end;
+
+----------------------------MOVIE------------------------------------------
+begin
+
+insert into movie values(seq_movie_id.nextval,'guntur karam',2000000000,1016,100, 100,5,interval '2:38' hour to minute,to_date('2024-01-12','YYYY-MM-dd'));
+
+insert into movie values(seq_movie_id.nextval,'Hi Nana',650000000,1017,111,107,5,interval '2:35' hour to minute,to_date('2023-12-07','YYYY-MM-dd'));
+
+insert into movie values(seq_movie_id.nextval,'Baahubali',1800000000,1005,109,108,2,interval '2:38' hour to minute,to_date('2015-07-10','YYYY-MM-dd'));
+ 
+insert into movie values(seq_movie_id.nextval,'Jersey',800000000,1002,105,107,10,interval '2:40' hour to minute,to_date('2019-04-19','YYYY-MM-dd'));  
+
+insert into movie values(seq_movie_id.nextval,'Hanuman',250000000,1004,123,109,1,interval '2:38' hour to minute,to_date('2024-01-12','YYYY-MM-dd'));
+  
+insert into movie values(seq_movie_id.nextval,'Mahanati',250000000,1008,115,118,4,interval '2:57' hour to minute,to_date('2018-05-09','YYYY-MM-dd'));
+  
+insert into movie values(seq_movie_id.nextval,'RRR',4000000000,1009,109,110,2,interval '3:7' hour to minute,to_date('2022-03-24','YYYY-MM-dd'));  
+
+insert into movie values(seq_movie_id.nextval,'Pushpa',35000000000,1004,136,111,4,interval '2:59' hour to minute,to_date('2021-12-17','YYYY-MM-dd')); 
+ 
+insert into movie values(seq_movie_id.nextval,'Sita Ramam',250000000,1008,127,115,1,interval '2:43' hour to minute,to_date('2022-07-05','YYYY-MM-dd')); 
+ 
+insert into movie values(seq_movie_id.nextval,'Master',1350000000,1010,145,112,1,interval '2:59' hour to minute,to_date('2021-01-13','YYYY-MM-dd'));  
+end;
+
+---------------------MOVIE_CREW-----------------------------
+begin
+insert into movie_crew values(seq_movie_crew_id.nextval,101,100,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,101,101,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,102,111,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,102,113,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,103,109,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,103,110,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,104,105,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,104,106,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,105,123,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,105,118,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,106,115,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,106,114,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,107,109,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,107,144,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,108,136,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,108,137,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,109,127,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,109,127,'producer');
+insert into movie_crew values(seq_movie_crew_id.nextval,110,145,'director');
+insert into movie_crew values(seq_movie_crew_id.nextval,110,142,'producer');
+end;
+
+-----------------------------------------MOVIE_ CAST-----------------------------
+begin
+insert into movie_cast values(seq_movie_cast_id.nextval,101,100,'Venkata Ramana');
+insert into movie_cast values(seq_movie_cast_id.nextval,101,101,'Amulya');
+insert into movie_cast values(seq_movie_cast_id.nextval,102,107,'Viraj');
+insert into movie_cast values(seq_movie_cast_id.nextval,102,119,'Yashna');
+insert into movie_cast values(seq_movie_cast_id.nextval,103,108,'Amarendra Baahubali');
+insert into movie_cast values(seq_movie_cast_id.nextval,103,120,'Anushka Shetty');
+insert into movie_cast values(seq_movie_cast_id.nextval,104,107,'Arjun');
+insert into movie_cast values(seq_movie_cast_id.nextval,104,121,'Sarah');
+insert into movie_cast values(seq_movie_cast_id.nextval,105,109,'Hanumanthu');
+insert into movie_cast values(seq_movie_cast_id.nextval,105,117,'Meenakshi');
+insert into movie_cast values(seq_movie_cast_id.nextval,106,118,'Savitri');
+insert into movie_cast values(seq_movie_cast_id.nextval,106,115,'Gemini Ganeran');
+insert into movie_cast values(seq_movie_cast_id.nextval,107,110,'Alluri sitrama raju');
+insert into movie_cast values(seq_movie_cast_id.nextval,107,122,'Sita');
+insert into movie_cast values(seq_movie_cast_id.nextval,108,111,'Pushpa Raj');
+insert into movie_cast values(seq_movie_cast_id.nextval,108,116,'Srivalli');
+insert into movie_cast values(seq_movie_cast_id.nextval,109,115,'Ram');
+insert into movie_cast values(seq_movie_cast_id.nextval,109,119,'PrincessNoorJahan');
+insert into movie_cast values(seq_movie_cast_id.nextval,110,112,'JD');
+insert into movie_cast values(seq_movie_cast_id.nextval,110,114,'Charulatha');
+end;
+
+------------------------USERS---------------------------------
+begin
+insert into users values (seq_users_id.nextval,'john',40,'MALE','john@123');
+insert into users values (seq_users_id.nextval,'Zona',35,'FEMALE','zona*&098');
+insert into users values (seq_users_id.nextval,'jack',50,'MALE','JACK#_@345');
+insert into users values (seq_users_id.nextval,'shyam roy',40,'MALE','SR123@34');
+insert into users values (seq_users_id.nextval,'Rosie',35,'FEMALE','Rose56_@_12');
+insert into users values (seq_users_id.nextval,'David',34,'MALE','Dav109#');
+insert into users values (seq_users_id.nextval,'abel',43,'MALE','ABEL@456');
+insert into users values (seq_users_id.nextval,'Sana',31,'FEMALE','Sana12&@34');
+insert into users values (seq_users_id.nextval,'Alexander',38,'MALE','ADR1267@*');
+insert into users values (seq_users_id.nextval,'Gracy',43,'FEMALE','gracy198');
+insert into users values (seq_users_id.nextval,'Aishwarya',32,'FEMALE','Aishu_@630');
+insert into users values (seq_users_id.nextval,'Raj',42,'MALE','Raju_@#123');
+insert into users values (seq_users_id.nextval,'Virat',38,'MALE','Virat_12@34');
+insert into users values (seq_users_id.nextval,'Ashwin',30,'MALE','Ash@win87');
+insert into users values (seq_users_id.nextval,'Sony',28,'FEMALE','Sony_1267@');
+insert into users values (seq_users_id.nextval,'jincy',27,'FEMALE','Jin@8431');
+end;
+
+-------------------------------RATINGS---------------
+begin
+insert into ratings values(seq_rating_id.nextval,3,101,4.4,'Entertaining');
+insert into ratings values(seq_rating_id.nextval,4,104,4.9,'Authentic,realistic drama movie with the game of cricket at its core');
+
+insert into ratings values(seq_rating_id.nextval,5,103,4.7,'visual effects,action sequences,music and performances became a record breaking box office sucess');
+
+insert into ratings values(seq_rating_id.nextval,6,102,4.8,'Family drama');
+insert into ratings values(seq_rating_id.nextval,7,105,4.9,'Teja sajja did a wonderful job');
+insert into ratings values(seq_rating_id.nextval,8,106,4.5,'classic best top notch performance to be remembered by keerthy');
+
+insert into ratings values(seq_rating_id.nextval,9,107,4.7,'Impressive');
+insert into ratings values(seq_rating_id.nextval,10,108,4.9,'pushpa is Alluarjun s one man show as the icon star delivers as knockout performance ');
+
+insert into ratings values(seq_rating_id.nextval,11,109,4.8,'speechless after watching this movie');
+insert into ratings values(seq_rating_id.nextval,12,109,4.2,'An Emotional lovestory with enough twists and a good message');
+
+insert into ratings values(seq_rating_id.nextval,13,110,4.0,'good');
+insert into ratings values(seq_rating_id.nextval,14,105,4.0,'Traditional genre of devotional storytelling');
+insert into ratings values(seq_rating_id.nextval,15,107,4.6,'Violent,dramatic');
+insert into ratings values(seq_rating_id.nextval,16,108,4.5,'alluarjun given very good performance as pushpa raj');
+
+insert into ratings values(seq_rating_id.nextval,17,106,4.9,'It was very heart touching and moving.It is fabulously directed by nagashwin');
+
+insert into ratings values(seq_rating_id.nextval,18,109,3.9,'on the whole sita ramam is a beautiful love story');
+end;
+
